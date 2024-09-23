3bf5d142ade71eec8ab43bd5e35d4cca1259317e
Valeriia <malvinacat0404@gmail.com>
2024-09-23 20:56:58 +0200

    11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,61 +1,21 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+const apiKey = '46143804-c250369a659784c87f3539553';
 
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault();  
-    const query = document.getElementById('searchQuery').value.trim();  
+export function searchImages(query) {
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-    if (query === "") {
-     
-        iziToast.error({
-            title: 'Error',
-            message: 'Search field cannot be empty!',
-            position: 'topRight'
-        });
-        return;
-    }
-    searchImages(query);
-});
-
-
-
-function searchImages(query) {
-    const apiKey = '46143804-c250369a659784c87f3539553';
-    const url = `https://pixabay.com/api/?key=${46143804-c250369a659784c87f3539553}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const imageResults = document.getElementById('imageResults');
-        imageResults.innerHTML = '';
-
-        if(data.hits && data.hits.length > 0) {
-            data.hits.array.forEach(image => {
-                const imgElement = document.createElementa('img');
-                imgElement.src = image.webformatURL;
-                imgElement.alt = image.tags;
-                imgElement.style.width = '200px';
-                imgElement.style.margin = '10px';
-                imageResults.appendChild(imgElement);
-            });
-        } else {
-            iziToast.error({
-                title: 'Error',
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                position: 'topRight'
-            });
-
+    return fetch(url)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error()
         }
+        return response.json();
+    })
+    .then(data => {
+        return data.hits;
     })
     .catch(error => {
-        iziToast.error({
-            title: 'Errpor',
-            message: 'Something went wrong. Please try again later!',
-            position: 'topRight'
-        });
         console.error('Error fetching images:', error);
+        throw error;
     });
 }
-
-
 
