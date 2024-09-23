b80b190ef2a948208d6189ae0300b569f0c8ebde
Valeriia <malvinacat0404@gmail.com>
2024-09-23 21:05:29 +0200

    21

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,22 @@
 const apiKey = '46143804-c250369a659784c87f3539553';
 
 export function searchImages(query) {
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+    query
+  )}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-    return fetch(url)
+  return fetch(url)
     .then(response => {
-        if(!response.ok) {
-            throw new Error()
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
     })
     .then(data => {
-        return data.hits;
+      return data.hits;
     })
     .catch(error => {
-        console.error('Error fetching images:', error);
-        throw error;
+      console.error('Error fetching images:', error);
+      throw error;
     });
 }
-
