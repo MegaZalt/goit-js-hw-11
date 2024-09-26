151ffa43ecd6ab3bfe8295babffafb30c7300470
Valeriia <malvinacat0404@gmail.com>
2024-09-26 18:57:52 +0200

    css and js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,18 +1,17 @@
-import simpleLightbox from "simplelightbox";
+import simpleLightbox from 'simplelightbox';
 
 export function renderGallery(images) {
-    const gallery = document.querySelector('.gallery');
-    gallery.innerHTML = '';
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
 
-    if(images.length === 0) {
-      gallery.innerHTML = '<p>Sorry, there are no images matching your search query. Please try again!</p>';
-      return;
-    }
+  if (images.length === 0) {
+    gallery.innerHTML =
+      '<p>Sorry, there are no images matching your search query. Please try again!</p>';
+    return;
+  }
 
-    const markup = images
-    .map(image => {
-      return `
-      <div class="gallery-item">
+  const markup = images.map(image => {
+    `<div class="gallery-item">
         <a href="${image.largeImageURL}">
           <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
         </a>
@@ -20,14 +19,12 @@ export function renderGallery(images) {
     })
     .join('');
 
-    gallery.innerHTML = markup;
+  gallery.innerHTML = markup;
 
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionsDelay: 250,
-    });
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+  });
 
-    lightbox.refresh();
-  }
-
-  
+  lightbox.refresh();
+}
